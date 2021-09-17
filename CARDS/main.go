f4269260feba92b27defbedde108347c5c1beb7f
Megha184 <meghaagrwal.18@gmail.com>
2021-09-17 10:35:53 +0530

    Cards Project till the converting to string part

--- CARDS/main.go
+++ CARDS/main.go
@@ -4,17 +4,28 @@ import "fmt"
 
 func main() {
 	//var card string = "Ace of Spades"
-	card := newCard() //figure out the type
+	//card := newCard() //figure out the type
 	//card:="five"
-	cards := []string{newCard(), "Ace"}
-	fmt.Print(cards) //slice of type string
-	cards = append(cards, "Six")
-	fmt.Println(card)
-	for i, c := range cards {
-		fmt.Println(i, c)
-	}
+
+	// cards := deck{newCard(), "Ace"}
+	// fmt.Print(cards) //slice of type string
+	// cards = append(cards, "Six")
+	// cards.print()
+	//fmt.Println(card)
+	// for i, c := range cards {
+	// 	fmt.Println(i, c)
+	// }
+	value := newDeck()
+	//value.print()
+	d, value := deal(value, 5)
+
+	d.print()
+	value.print()
+	v := toString(value)
+	fmt.Print(v)
 
 }
-func newCard() string {
-	return "five"
-}
+
+// func newCard() string {
+// 	return "five"
+// }
--- helper.go
+++
@@ -1,7 +0,0 @@
-package helper
-
-import "fmt"
-
-func main() {
-	fmt.Print("hey")
-}
